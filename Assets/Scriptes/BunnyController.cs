7c4fc238b37b90b395579756df3ce1b9a686717c
Cremate91 <andreas.burger91@gmail.com>
2016-06-15 21:52:01 +0200

    Add bunny animations.

--- Assets/Scriptes/BunnyController.cs
+++ Assets/Scriptes/BunnyController.cs
@@ -6,16 +6,20 @@ public class BunnyController : MonoBehaviour {
 	public float bunnyJumpForce = 500f;
 
 	private Rigidbody2D rb;
+	private Animator myAnim;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+		myAnim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonUp ("Jump")) {
 			rb.AddForce (transform.up * bunnyJumpForce);
+
 		}
+		myAnim.SetFloat ("vVelocity", Mathf.Abs(rb.velocity.y));
 	}
 }
